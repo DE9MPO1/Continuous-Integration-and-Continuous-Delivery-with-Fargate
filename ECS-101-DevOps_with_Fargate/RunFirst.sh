514c3a8dee74e22e29098c440aaec2dac1a05179
williamlin0504 <macterwilliam0504@gmail.com>
2019-02-20 16:54:27 +0800

    switch workshop-commit to fargate-commit

--- ECS-101-DevOps_with_Fargate/RunFirst.sh
+++ ECS-101-DevOps_with_Fargate/RunFirst.sh
@@ -1,7 +1,7 @@
-git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/workshop-commit
-cp -R ./Continuous-Integration-and-Continuous-Delivery-with-Fargate/ECS-101-DevOps_with_Fargate/* ./workshop-commit
+git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/fargate-commit
+cp -R ./Continuous-Integration-and-Continuous-Delivery-with-Fargate/ECS-101-DevOps_with_Fargate/* ./fargate-commit
 rm -rf Continuous-Integration-and-Continuous-Delivery-with-Fargate
-cd workshop-commit
+cd fargate-commit
 rm -rf images
 git add .
 git commit -m “workshop-bash”
